34bb6622c44e570568cc7f2a828050666efe6ae7
Revmirych <145692342+Revmirych@users.noreply.github.com>
2025-01-08 16:37:47 +0300

    Create start.sh

---
+++ Webpack/start.sh
@@ -0,0 +1,6 @@
+mkdir my-webpack-project
+cd my-webpack-project
+npm init -y
+npm install webpack webpack-cli --save-dev
+npm install babel-loader @babel/core @babel/preset-env --save-dev
+npm install style-loader css-loader --save-dev
